2d0a4f64d9476575e01b85c5b428554f6e9df3c0
admin <m18820136354_1@163.com>
2019-08-22 11:28:13 +0800

    观察者模式+发布订阅模式

--- patternJs/ObserverPattern.js
+++ patternJs/ObserverPattern.js
@@ -104,3 +104,248 @@
     // Append the item to the container
     container.appendChild( check );
   }
+
+
+// 一个如何使用发布/订阅的示例，如果提供了功能实现的强大功能publish()，subscribe()以及unsubscribe()后台
+// 这里的一般想法是促进松散耦合。它们不是单个对象直接调用其他对象的方法，而是订阅另一个对象的特定任务或活动，并在发生时通知。
+// A very simple new mail handler
+// A count of the number of messages received
+var mailCounter = 0;
+ 
+// Initialize subscribers that will listen out for a topic
+// with the name "inbox/newMessage".
+ 
+// Render a preview of new messages
+var subscriber1 = subscribe( "inbox/newMessage", function( topic, data ) {
+ 
+  // Log the topic for debugging purposes
+  console.log( "A new message was received: ", topic );
+ 
+  // Use the data that was passed from our subject
+  // to display a message preview to the user
+  $( ".messageSender" ).html( data.sender );
+  $( ".messagePreview" ).html( data.body );
+ 
+});
+ 
+// Here's another subscriber using the same data to perform
+// a different task.
+ 
+// Update the counter displaying the number of new
+// messages received via the publisher
+ 
+var subscriber2 = subscribe( "inbox/newMessage", function( topic, data ) {
+ 
+  $('.newMessageCounter').html( ++mailCounter );
+ 
+});
+ 
+publish( "inbox/newMessage", [{
+  sender: "hello@google.com",
+  body: "Hey there! How are you doing today?"
+}]);
+ 
+// We could then at a later point unsubscribe our subscribers
+// from receiving any new topic notifications as follows:
+// unsubscribe( subscriber1 );
+// unsubscribe( subscriber2 );
+
+
+// 发布/订阅实现
+// Publish
+ 
+// jQuery: $(obj).trigger("channel", [arg1, arg2, arg3]);
+$( el ).trigger( "/login", [{username:"test", userData:"test"}] );
+ 
+// Dojo: dojo.publish("channel", [arg1, arg2, arg3] );
+dojo.publish( "/login", [{username:"test", userData:"test"}] );
+ 
+// YUI: el.publish("channel", [arg1, arg2, arg3]);
+el.publish( "/login", {username:"test", userData:"test"} );
+ 
+ 
+// Subscribe
+ 
+// jQuery: $(obj).on( "channel", [data], fn );
+$( el ).on( "/login", function( event ){ } );
+ 
+// Dojo: dojo.subscribe( "channel", fn);
+var handle = dojo.subscribe( "/login", function(data){ } );
+ 
+// YUI: el.on("channel", handler);
+el.on( "/login", function( data ){ } );
+ 
+ 
+// Unsubscribe
+ 
+// jQuery: $(obj).off( "channel" );
+$( el ).off( "/login" );
+ 
+// Dojo: dojo.unsubscribe( handle );
+dojo.unsubscribe( handle );
+ 
+// YUI: el.detach("channel");
+el.detach( "/login" );
+
+
+
+var pubsub = {};
+ 
+(function(myObject) {
+ 
+    // Storage for topics that can be broadcast
+    // or listened to
+    var topics = {};
+ 
+    // A topic identifier
+    var subUid = -1;
+ 
+    // Publish or broadcast events of interest
+    // with a specific topic name and arguments
+    // such as the data to pass along
+    myObject.publish = function( topic, args ) {
+ 
+        if ( !topics[topic] ) {
+            return false;
+        }
+ 
+        var subscribers = topics[topic],
+            len = subscribers ? subscribers.length : 0;
+ 
+        while (len--) {
+            subscribers[len].func( topic, args );
+        }
+ 
+        return this;
+    };
+ 
+    // Subscribe to events of interest
+    // with a specific topic name and a
+    // callback function, to be executed
+    // when the topic/event is observed
+    myObject.subscribe = function( topic, func ) {
+ 
+        if (!topics[topic]) {
+            topics[topic] = [];
+        }
+ 
+        var token = ( ++subUid ).toString();
+        topics[topic].push({
+            token: token,
+            func: func
+        });
+        return token;
+    };
+ 
+    // Unsubscribe from a specific
+    // topic, based on a tokenized reference
+    // to the subscription
+    myObject.unsubscribe = function( token ) {
+        for ( var m in topics ) {
+            if ( topics[m] ) {
+                for ( var i = 0, j = topics[m].length; i < j; i++ ) {
+                    if ( topics[m][i].token === token ) {
+                        topics[m].splice( i, 1 );
+                        return token;
+                    }
+                }
+            }
+        }
+        return this;
+    };
+}( pubsub ));
+// 使用我们的实施
+// Another simple message handler
+ 
+// A simple message logger that logs any topics and data received through our
+// subscriber
+var messageLogger = function ( topics, data ) {
+  console.log( "Logging: " + topics + ": " + data );
+};
+
+// Subscribers listen for topics they have subscribed to and
+// invoke a callback function (e.g messageLogger) once a new
+// notification is broadcast on that topic
+var subscription = pubsub.subscribe( "inbox/newMessage", messageLogger );
+
+// Publishers are in charge of publishing topics or notifications of
+// interest to the application. e.g:
+
+pubsub.publish( "inbox/newMessage", "hello world!" );
+
+// or
+pubsub.publish( "inbox/newMessage", ["test", "a", "b", "c"] );
+
+// or
+pubsub.publish( "inbox/newMessage", {
+sender: "hello@google.com",
+body: "Hey again!"
+});
+
+// We can also unsubscribe if we no longer wish for our subscribers
+// to be notified
+pubsub.unsubscribe( subscription );
+
+// Once unsubscribed, this for example won't result in our
+// messageLogger being executed as the subscriber is
+// no longer listening
+pubsub.publish( "inbox/newMessage", "Hello! are you still there?" );
+
+
+//用户界面通知
+//当数据模型更改时，应用程序将需要更新网格和计数器。在这种情况下，我们的主题（将发布主题/通知）是数据模型，我们的订阅者是网格和计数器。
+// Return the current local time to be used in our UI later
+getCurrentTime = function (){
+ 
+  var date = new Date(),
+        m = date.getMonth() + 1,
+        d = date.getDate(),
+        y = date.getFullYear(),
+        t = date.toLocaleTimeString().toLowerCase();
+
+       return (m + "/" + d + "/" + y + " " + t);
+};
+
+// Add a new row of data to our fictional grid component
+function addGridRow( data ) {
+  // ui.grid.addRow( data );
+  console.log( "updated grid component with:" + data );
+}
+
+// Update our fictional grid to show the time it was last
+// updated
+function updateCounter( data ) {
+  // ui.grid.updateLastChanged( getCurrentTime() );
+  console.log( "data last updated at: " + getCurrentTime() + " with " + data);
+}
+
+// Update the grid using the data passed to our subscribers
+gridUpdate = function( topic, data ){
+ if ( data !== undefined ) {
+    addGridRow( data );
+    updateCounter( data );
+  }
+};
+
+// Create a subscription to the newDataAvailable topic
+var subscriber = pubsub.subscribe( "newDataAvailable", gridUpdate );
+
+// The following represents updates to our data layer. This could be
+// powered by ajax requests which broadcast that new data is available
+// to the rest of the application.
+
+// Publish changes to the gridUpdated topic representing new entries
+pubsub.publish( "newDataAvailable", {
+ summary: "Apple made $5 billion",
+ identifier: "APPL",
+ stockPrice: 570.91
+});
+
+pubsub.publish( "newDataAvailable", {
+ summary: "Microsoft made $20 million",
+ identifier: "MSFT",
+ stockPrice: 30.85
+});
+
+
+// 使用Ben Alman的Pub / Sub实现解耦应用程序
